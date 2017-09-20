c711573d415d948578a7855448d3a7cd9be7206a
Surdjak <n64player@live.com>
2017-09-20 14:18:50 +0200

    Modification du message de hw.sh

--- hw.sh
+++ hw.sh
@@ -1,1 +1,1 @@
-echo "Hello Lucas Goareguer, comment vas-tu?\nPersonnellement, je vais très bien. J'ai récemment appris en cours à utiliser un dépôt GitHub, mais le plus drôle, c'est que mon nouveau binôme te ressemble beaucoup !"
+echo "lol"
